39112b1d535a14e19a6c4dd58a27ebb1d3082e98
notSoundBanana <chemaev.ds@gmail.com>
2022-09-16 19:24:01 +0300

    Added monster attack and mirrorer && healer skills

--- hw-2022-09-10/TextGame.playground/Contents.swift
+++ hw-2022-09-10/TextGame.playground/Contents.swift
@@ -1,8 +1,8 @@
 import Darwin
 
-let sleepTime = UInt32(2)
-//var monsterNum = Int.random(in: 3...20)
-var monsterNum = 2
+let sleepTime = UInt32(1)
+//var monsterNum = Int.random(in: 3...10)
+var monsterNum = 4
 
 class Player{
     let name: String
@@ -10,6 +10,8 @@ class Player{
     var attackPower = 20
     var defencePower = 10
     var playerType: playerType
+    var haveSword = false
+    var haveShield = false
     
     init(name: String, type: Int){
         self.name = name
@@ -23,29 +25,44 @@ class Player{
     }
     
     func attack(monster: Monster){
-        if playerType == .kevin{
+        if (playerType == .kevin){  // Способность Кевина
             var fatality = false
             let chance = Int.random(in: 1...10)
             switch chance{
-                case 1, 2: fatality = true
+                case 1: fatality = true
                 default: fatality = false
             }
             
             if fatality{
                 monster.hp -= monster.hp
-                print("ВЫ НАНЕСЛИ КРИТИЧЕСКИЙ УДАР!")
+                print("*ВЫ НАНЕСЛИ КРИТИЧЕСКИЙ УДАР!*")
+                
             }
             else{
                 monster.hp -= self.attackPower
-                print("Вы нанесли монстру \(self.attackPower) урона")
+                print("*Вы нанесли монстру \(self.attackPower) урона*")
                 print("")
             }
             
         }
         
+        else if (playerType == .mirrorer){  // Способность отражателя
+            let chance = Int.random(in: 1...10)
+        
+            if (chance <= 3){
+                print("ВЫ СМОГЛИ ОТРАЗИТЬ АТАКУ МОНСТРА")
+                print("")
+            }
+            else{
+                monster.hp -= self.attackPower
+                print("*Вы нанесли монстру \(self.attackPower) урона*")
+                print("")
+            }
+        }
+        
         else {
             monster.hp -= self.attackPower
-            print("Вы нанесли монстру \(self.attackPower) урона")
+            print("*Вы нанесли монстру \(self.attackPower) урона*")
             print("")
         }
         
@@ -94,10 +111,17 @@ class Monster{
             self.attackPower = 30
         }
     }
+    
+    func attack(player: Player){
+        player.hp -= self.attackPower
+        print("*Вам нанесли \(self.attackPower) урона*")
+        print("")
+    }
 }
 
 func printGeneralStatistics(){
     print("МОНСТРОВ ОСТАЛОСЬ: \(monsterNum)")
+    print("")
 }
 
 func printStatistics(player: Player, monster: Monster){
@@ -112,40 +136,51 @@ func printStatistics(player: Player, monster: Monster){
 let greeting1 = """
 Приветствую тебя в игре gameName, выбери персонажа:
 1. Отражатель (имеет 30% шанс полностью отразить атаку противника)
-2. Лекарь (восполняет 30% здоровья после каждого боя)
-3. Кевин (имеет 20% шанс убить противника с одного удара)
+2. Лекарь (восполняет 20 единиц здоровья после того, как у вас осталось <=30 единиц здоровья)
+3. Кевин (имеет 10% шанс убить противника с одного удара)
 
 """
 
 print(greeting1)
-
 sleep(sleepTime)
-
 var menu = Int.random(in: 1...3)
-
-
-    
 var player = Player(name: "Player1", type: menu)
-
 
 let greeting2 = """
 Вы выбрали тип \(menu)
 СЕГОДНЯ ВАМ ПРЕДСТОИТ ОДОЛЕТЬ \(monsterNum) МОНСТРОВ
 Игра начинается...
-
 """
-
 print(greeting2)
 print("--------------------------------------------------------------------------------")
+sleep(sleepTime)
+
 
 while (player.hp > 0 && monsterNum != 0){
-    let chance = Int.random(in: 1...2)
-    if (chance == 1){
+    let chance = Int.random(in: 1...20)
+    if (chance <= 10){
         print("Вы идете в поисках монстра...")
         sleep(sleepTime)
     }
-    else if (chance == 2){
+    
+    if (chance == 1 && player.haveSword == false){
+        print("Вы нашли мечь, сила удара увеличилась")
+        player.haveSword = true
+        player.attackPower += 30
+        sleep(sleepTime)
+    }
+    
+    if (chance == 2 && player.haveShield == false){
+        print("Вы нашли щит, количество получаемого урона уменьшится")
+        player.haveShield = true
+        player.defencePower += 30
+        sleep(sleepTime)
+    }
+    
+    
+    else if (chance > 10){
         print("ВЫ ВСТРЕТИЛИ МОНСТРА, ВСТУПАЕМ В БОЙ!")
+        print("")
         var monster: Monster
         let monsterDifficulty = Int.random(in: 1...100)
         let type: String
@@ -164,6 +199,7 @@ while (player.hp > 0 && monsterNum != 0){
             type = "Сложный"
         }
         print("Перед вами \(type) монстр")
+        print("")
         printStatistics(player: player, monster: monster)
         sleep(sleepTime)
         
@@ -174,6 +210,7 @@ while (player.hp > 0 && monsterNum != 0){
             if (monster.hp <= 0){
                 monsterNum -= 1
                 print("ВЫ УБИЛИ МОНСТРА")
+                print("")
                 sleep(sleepTime)
                 printGeneralStatistics()
                 sleep(sleepTime)
@@ -185,9 +222,40 @@ while (player.hp > 0 && monsterNum != 0){
                 }
                 break
             }
+            
+            printStatistics(player: player, monster: monster)
+            sleep(sleepTime)
+            
+            monster.attack(player: player)
+            
+            if (player.hp <= 0){
+                print("ВАС УБИЛИ")
+                sleep(sleepTime)
+                break
+            }
+
+            if (player.playerType == .healer && player.hp <= 30){  // Способность лекаря
+                player.hp += 20
+                print("*Вы восстановили 20 единиц здоровья*")
+            }
+            
             printStatistics(player: player, monster: monster)
             sleep(sleepTime)
         }
-        
     }
 }
+
+
+print("--------------------------------------------------------------------------------")
+print("ИГРА ОКОНЧЕНА")
+
+
+if (player.hp <= 0){
+    print("ВЫ ПРОИГРАЛИ!")
+}
+else if (monsterNum == 0){
+    print("ВЫ ВЫИГРАЛИ!")
+}
+
+
+// !TODO Сделать способность у монстров
